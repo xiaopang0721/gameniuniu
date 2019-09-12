876f0cb6c4f7e6fac536338e5a2c2ead4134aee8
xiaoer <xiaoer@example.com>
2019-09-12 15:02:35 +0800

    client批量提交

--- page/NiuNiuMapPage.ts
+++ page/NiuNiuMapPage.ts
@@ -1167,7 +1167,7 @@ module gameniuniu.page {
                         TongyongPageDef.ins.alertRecharge(StringU.substitute("老板，您的金币少于{0}哦~\n补充点金币去大杀四方吧~", this._room_config[1]), () => {
                             this._game.uiRoot.general.open(DatingPageDef.PAGE_CHONGZHI);
                         }, () => {
-                        }, false, TongyongPageDef.TIPS_SKIN_STR["cz"]);
+                        }, true, TongyongPageDef.TIPS_SKIN_STR["cz"]);
                     }
 
                     break;
@@ -1327,7 +1327,7 @@ module gameniuniu.page {
                             TongyongPageDef.ins.alertRecharge(StringU.substitute("老板，您的金币少于{0}哦~\n补充点金币去大杀四方吧~", this._room_config[1]), () => {
                                 this._game.uiRoot.general.open(DatingPageDef.PAGE_CHONGZHI);
                             }, () => {
-                            }, false, TongyongPageDef.TIPS_SKIN_STR["cz"]);
+                            }, true, TongyongPageDef.TIPS_SKIN_STR["cz"]);
                             return;
                         }
                     }
